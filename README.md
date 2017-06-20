a6fd2083d70c3e94fb251c234fe1d4ca24164f7d
Ram Lmn <ramlmn@outlook.com>
2017-06-20 20:03:37 +0530

    Sensible reshape
    * Change license to MIT
    
    * Little perf improvements
    
    * Update focusible element string list
    
    * Update README

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A library to help trap the focus of keyboard inside an element.
 
 > **Warn:** Don't trap the users keyboard unless the usecase is so, it might be a usability issue rather
 > than providing accessibility
+>
 > Refer to [https://youtu.be/JS68faEUduk?t=10m56s](https://youtu.be/JS68faEUduk?t=10m56s)
 >
 > You'r usecase might be suited by [inert](https://github.com/WICG/inert) [https://youtu.be/fGLp_gfMMGU](https://youtu.be/fGLp_gfMMGU)
@@ -13,25 +14,25 @@ A library to help trap the focus of keyboard inside an element.
 Using the `TrapKeyboard` class:
 
 ``` js
-let trap = new TrapKeyboard(element);
+const trap = new TrapKeyboard(root, options);
 
 // Call the 'trap' method
 trap.trap();
 ```
 
-The `TrapKeyboard` class takes a DOM node(`element`) as its parameter.
+### Arguments
+`root`: Element to trap keyboard inside of.
 
-Creating an object for `TrapKeyboard` class would by default call the `trap` method. Pass the
-second argument as `false` for it to not.
+`options`: [MutationObserverInit](https://developer.mozilla.org/en/docs/Web/API/MutationObserver#MutationObserverInit) object for responding to changes on root and child elements.
 
-To untrap the keyboard, just call the `unTrap` method on the reference object:
+To untrap the keyboard, just call the `unTrap` method on the instance:
 ``` js
 trap.unTrap();
 ```
 
 ## License:
 
-[Apache 2.0](/LICENSE)
+[MIT](/LICENSE)
 
 ## Inspired from
 [https://www.youtube.com/watch?v=BoAsayPVogE](https://www.youtube.com/watch?v=BoAsayPVogE)
--- trap-keyboard.js
+++ trap-keyboard.js
@@ -1,115 +1,170 @@
-'use strict';
+(function() {
+  'use strict';
 
-class TrapKeyboard {
-  constructor(container, trapDefault = true) {
-    // Check if the container element is a DOM node
-    if (!(container.nodeType === 1)) {
-      return new TypeError('Element provided is not ElementNode');
+  /**
+   * A class which helps to trap user input inside an element
+   *
+   * @class TrapKeyboard
+   */
+  class TrapKeyboard {
+    /**
+     * Creates an instance of TrapKeyboard
+     * @param {Node} element
+     * @param {Object} [options={}]
+     * @memberof TrapKeyboard
+     */
+    constructor(element, options = {}) {
+      // Check if the element is a DOM node
+      if (!(element.nodeType === 1)) {
+        return new TypeError('Provide an ElementNode');
+      }
+
+      this._root = element;
+
+      this._firstTabStop = null;
+      this._lastTabStop = null;
+
+      this._onKeyDown = this._onKeyDown.bind(this);
+      this._onMutation = this._onMutation.bind(this);
+
+      const observerOptions = Object.assign({}, {
+        childList: true,
+        attributes: true,
+        subtree: true,
+      }, options);
+
+      // Add a MutationObserver to watch for DOM changes
+      this.observer = new MutationObserver(this._onMutation);
+
+      // Start the observer
+      this.observer.observe(this._root, observerOptions);
+
+      // Need to update on window resize if any CSS Media Queries might change
+      // the visibility and display of focusable elements
+      window.addEventListener('resize', this._onMutation);
+
+      // Initial setup
+      this._onMutation();
     }
 
-    this.container = container;
-
-    // Query all possible focusable elements
-    this.focusableElementsString = [
-      'a[href]',
-      'area[href]',
-      'button:not([disabled])',
-      'input:not([disabled])',
-      'select:not([disabled])',
-      'textarea:not([disabled])',
-      'iframe',
-      'object',
-      'embed',
-      '[tabindex]:not([tabindex^="-"])',
-      '[contenteditable]',
-      '[aria-hidden=false]',
-    ].join();
-
-    this.trapTabKey = this.trapTabKey.bind(this);
-    this.onMutation = this.onMutation.bind(this);
-
-    // Add a MutationObserver to watch for DOM changes
-    this.observer = new MutationObserver(this.onMutation);
-
-    // Start the observer
-    this.observer.observe(this.container, {
-      childList: true,
-      attributes: true,
-      subtree: true,
-    });
-
-    // Need to update on window resize if any CSS Media Queries might change
-    // the visibility and display of focusable elements
-    window.addEventListener('resize', this.onMutation);
-
-    if (trapDefault) {
-      this.trap();
-    }
-  }
-
-  onMutation() {
-    let elements = Array.from(
-        this.container.querySelectorAll(this.focusableElementsString)
+    _onMutation() {
+      // Get all the possible elements that the user can interact with
+      const elements = Array.from(
+        this._root.querySelectorAll(this.constructor.focusableElementsString)
       );
-    this.focusableElements = [];
 
-    elements.forEach(el => {
-      let gcs = getComputedStyle(el);
-
-      // Check if the element is accessible
-      // Note: Not an ideal solution for checking visibility and display
-      if (!(gcs.display === 'none')
-        || !(gcs.visibility === 'hidden')
-        || !(el.getAttribute('tabindex') === -1)) {
-        this.focusableElements.push(el);
-      }
-    });
-
-    // Get the first and last tab stops to act as anchor points
-    this.firstTabStop = this.focusableElements[0] || this.container;
-    this.lastTabStop =
-      this.focusableElements[this.focusableElements.length - 1] || this.container;
-  }
-
-  trapTabKey(e) {
-    // No elements to focus
-    if (this.focusableElements.length === 0) {
-      e.preventDefault();
-      return;
+      // Get the focusible element from top bottom
+      this._firstTabStop = this._getFocusibleElement(elements);
+      // Get the focusible element from bottom up
+      this._lastTabStop = this._getFocusibleElement(elements.reverse());
     }
 
-    // Check for TAB key press
-    if (e.keyCode === 9) {
-      // SHIFT + TAB
-      if (e.shiftKey) {
-        if (document.activeElement === this.firstTabStop) {
-          e.preventDefault();
-          this.lastTabStop.focus();
-        }
+    _getFocusibleElement(list) {
+      // Find a 'focusible' element
+      return list.find(element => {
+        // Get the computed styles of the element
+        const styles = getComputedStyle(element);
 
-      // TAB
+        // Check if the element is accessible
+        // Not an ideal solution though
+        if (!(styles.display === 'none') || !(styles.visibility === 'hidden')) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+
+    _onKeyDown(event) {
+      // We need atleast two unique elements for it to work
+      if (!(this._firstTabStop && this._lastTabStop)) {
+        event.preventDefault();
+        return;
+      }
+
+      // If we are on the last element transfer the focus to first element and vice versa
+      // Check for TAB key press
+      if (event.keyCode === 9) {
+        // SHIFT + TAB
+        if (event.shiftKey) {
+          if (document.activeElement === this._firstTabStop) {
+            // Traverse back
+            event.preventDefault();
+            this._lastTabStop.focus();
+          }
+        } else {
+          // TAB
+          if (document.activeElement === this._lastTabStop) {
+            // Traverse forward
+            event.preventDefault();
+            this._firstTabStop.focus();
+          }
+        }
+      }
+    }
+
+    /**
+     * Trap the users keyboard, this effects until unTrap is called
+     *
+     * @memberof TrapKeyboard
+     */
+    trap() {
+      // Add a keydown event
+      this._root.addEventListener('keydown', this._onKeyDown);
+
+      // Focus the first element, if any
+      if (this._firstTabStop) {
+        this._firstTabStop.focus();
       } else {
-        if (document.activeElement === this.lastTabStop) {
-          e.preventDefault();
-          this.firstTabStop.focus();
-        }
+        this._root.focus();
       }
+    }
+
+
+    /**
+     * Stop trapping the users keyboard
+     *
+     * @memberof TrapKeyboard
+     */
+    unTrap() {
+      // Remove the event listeners
+      this._root.removeEventListener('keydown', this._onKeyDown);
+    }
+
+
+    /**
+     * CSS query string for possbile focusible elements
+     *
+     * @readonly
+     * @static
+     * @memberof TrapKeyboard
+     */
+    static get focusableElementsString() {
+      // All possible focusable elements
+      return [
+        'a[href]',
+        'area[href]',
+        'button:not([disabled])',
+        'input:not([disabled])',
+        'select:not([disabled])',
+        'textarea:not([disabled])',
+        'datalist:not([disabled])',
+        'iframe',
+        'object',
+        'embed',
+        '[tabindex]:not([tabindex="-1"])',
+        '[contenteditable]',
+      ].join();
     }
   }
 
-  trap() {
-    // Add a keydown event
-    this.container.addEventListener('keydown', this.trapTabKey);
-
-    // Call this once, to setup things
-    this.onMutation();
-
-    // Focus the first element
-    this.firstTabStop.focus();
+  if (typeof module != 'undefined' && module.exports) {
+    module.exports = TrapKeyboard;
+  } else if (typeof define === 'function' && define.amd) {
+    define('TrapKeyboard', [], function() {
+      return TrapKeyboard;
+    });
+  } else {
+    self.TrapKeyboard = TrapKeyboard;
   }
-
-  unTrap() {
-    // Remove the event listeners
-    this.container.removeEventListener('keydown', this.trapTabKey);
-  }
-}
+}());
